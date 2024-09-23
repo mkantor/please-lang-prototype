488bffb1b2cdea24e3b802422c0116c874f89d3c
Matt Kantor <the.matt.kantor@gmail.com>
2024-09-23 11:38:17 -0400

    Read from stdin, write to stdout

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello, world!')
+async function read(stream: AsyncIterable<string>): Promise<string> {
+  let source: string = ''
+  for await (const chunk of stream) {
+    source += chunk
+  }
+  return source
+}
+
+process.stdout.write(await read(process.stdin))
