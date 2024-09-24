27b5f05b1f9c2a917f0d5b2635b3bc2f92f01dd6
Matt Kantor <the.matt.kantor@gmail.com>
2024-09-24 09:35:16 -0400

    Parse input as JSON, prettify output

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import * as util from 'node:util'
+
 const read = async (stream: AsyncIterable<string>): Promise<string> => {
   let input: string = ''
   for await (const chunk of stream) {
@@ -7,7 +9,29 @@ const read = async (stream: AsyncIterable<string>): Promise<string> => {
 }
 
 const main = async (process: NodeJS.Process): Promise<undefined> => {
-  process.stdout.write(await read(process.stdin))
+  const rawInput = await read(process.stdin)
+  const parsedInput: unknown = JSON.parse(rawInput)
+
+  process.stdout.write(
+    util.inspect(parsedInput, {
+      colors: true,
+      depth: Infinity,
+    }),
+  )
+
+  // Writing a newline ensures that output is flushed before exiting. Otherwise there may be no
+  // output actually printed to the console. This happens whether or not `process.exit` is
+  // explicitly called. See:
+  //  - <https://github.com/nodejs/node/issues/6379>
+  //  - <https://github.com/nodejs/node/issues/6456>
+  //  - <https://github.com/nodejs/node/issues/2972>
+  //  - …and many other near-duplicate issues
+  //
+  // I've tried other workarounds such as passing a callback to `process.stdout.write` and ensuring
+  // the returned `Promise` is not resolved until it is called, explicitly calling
+  // calling `process.stdout.end` and/or `process.stdout.uncork`, etc and so far this is the only
+  // one that reliably results in the desired behavior.
+  process.stdout.write('\n')
 }
 
 await main(process)
