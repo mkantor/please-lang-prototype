eb681ce66e94297e14d742d6e10c8f51eed8f46b
Matt Kantor <the.matt.kantor@gmail.com>
2024-09-24 13:07:36 -0400

    Add `Option` and `Either` ADTs

---
+++ src/adts/either.ts
@@ -0,0 +1,37 @@
+const tagKey = Symbol('either')
+export type Either<Left, Right> =
+  | {
+      readonly [tagKey]: 'left'
+      readonly value: Left
+    }
+  | {
+      readonly [tagKey]: 'right'
+      readonly value: Right
+    }
+
+export const makeLeft = <Value>(value: Value) =>
+  ({
+    [tagKey]: 'left',
+    value,
+  } satisfies Either<Value, never>)
+
+export const makeRight = <Value>(value: Value) =>
+  ({
+    [tagKey]: 'right',
+    value,
+  } satisfies Either<never, Value>)
+
+export const match = <Left, Right, Result>(
+  adt: Either<Left, Right>,
+  cases: {
+    left: (value: Left) => Result
+    right: (value: Right) => Result
+  },
+): Result => {
+  switch (adt[tagKey]) {
+    case 'left':
+      return cases[adt[tagKey]](adt.value)
+    case 'right':
+      return cases[adt[tagKey]](adt.value)
+  }
+}
---
+++ src/adts/option.ts
@@ -0,0 +1,32 @@
+const tagKey = Symbol('option')
+export type Option<Value> =
+  | {
+      readonly [tagKey]: 'none'
+    }
+  | {
+      readonly [tagKey]: 'some'
+      readonly value: Value
+    }
+
+export const none = { [tagKey]: 'none' } satisfies Option<never>
+
+export const makeSome = <Value>(value: Value) =>
+  ({
+    [tagKey]: 'some',
+    value,
+  } satisfies Option<Value>)
+
+export const match = <Value, Result>(
+  adt: Option<Value>,
+  cases: {
+    none: () => Result
+    some: (value: Value) => Result
+  },
+): Result => {
+  switch (adt[tagKey]) {
+    case 'none':
+      return cases[adt[tagKey]]()
+    case 'some':
+      return cases[adt[tagKey]](adt.value)
+  }
+}
