d2cecb6a082a27a91b13ed55ed96163e2377c34b
Matt Kantor <the.matt.kantor@gmail.com>
2024-09-25 13:59:49 -0400

    Adopt TypeBox

--- src/parsing/molecule.ts
+++ src/parsing/molecule.ts
@@ -1,35 +1,26 @@
+import {
+  Type as JSONSchema,
+  type Static as TypeOfJSONSchema,
+} from '@sinclair/typebox'
+import { Value as JSONSchemaValue } from '@sinclair/typebox/value'
 import * as option from '../adts/option.js'
 import { type Option } from '../adts/option.js'
-import * as atom from './atom.js'
-import { type Atom } from './atom.js'
 
-export type Molecule = {
-  readonly [key: Atom]: Atom | Molecule
-}
+const moleculeSchema = JSONSchema.Recursive(moleculeSchema =>
+  JSONSchema.Record(
+    JSONSchema.String(),
+    JSONSchema.Union([JSONSchema.String(), moleculeSchema]),
+  ),
+)
+
+export type Molecule = TypeOfJSONSchema<typeof moleculeSchema>
 
 // TODO: evolve this into `validateMolecule`, returning an `Either` with error details on the left
 export const asMolecule = (potentialMolecule: unknown): Option<Molecule> => {
-  if (
-    potentialMolecule === null ||
-    typeof potentialMolecule !== 'object' ||
-    Array.isArray(potentialMolecule)
-  ) {
+  try {
+    const molecule = JSONSchemaValue.Decode(moleculeSchema, potentialMolecule)
+    return option.makeSome(molecule)
+  } catch (_error) {
     return option.none
   }
-
-  for (let [key, value] of Object.entries(potentialMolecule)) {
-    if (!atom.isAtom(key) || (!atom.isAtom(value) && !isMolecule(value))) {
-      return option.none
-    }
-    ;({ [key]: value }) satisfies Molecule // sanity check (since there's an assertion below)
-  }
-
-  // If execution gets here `potentialMolecule` must be a valid `Molecule`.
-  return option.makeSome(potentialMolecule as Molecule)
 }
-
-const isMolecule = (value: unknown): value is Molecule =>
-  option.match(asMolecule(value), {
-    none: () => false,
-    some: _ => true,
-  })
