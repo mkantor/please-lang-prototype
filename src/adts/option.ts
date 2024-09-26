9ac874018e6b171b358573b96014eb7066d33113
Matt Kantor <the.matt.kantor@gmail.com>
2024-09-26 12:26:59 -0400

    Add `option.flatMap`

--- src/adts/option.ts
+++ src/adts/option.ts
@@ -16,6 +16,11 @@ export const makeSome = <Value>(value: Value) =>
     value,
   } satisfies Option<Value>)
 
+export const flatMap = <Value, NewValue>(
+  option: Option<Value>,
+  f: (value: Value) => Option<NewValue>,
+): Option<NewValue> => match(option, { none: () => none, some: f })
+
 export const match = <Value, Result>(
   adt: Option<Value>,
   cases: {
