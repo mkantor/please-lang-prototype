7135ce99b0811933e41258f9844b14bea66ca6b7
Matt Kantor <the.matt.kantor@gmail.com>
2024-10-10 09:18:37 -0400

    Improve a comment

--- src/index.ts
+++ src/index.ts
@@ -48,18 +48,17 @@ const main = async (process: NodeJS.Process): Promise<undefined> => {
     },
   })
 
-  // Writing a newline ensures that output is flushed before exiting. Otherwise there may be no
-  // output actually printed to the console. This happens whether or not `process.exit` is
-  // explicitly called. See:
+  // Writing a newline ensures that output is flushed before terminating, otherwise nothing may be
+  // printed to the console. See:
   //  - <https://github.com/nodejs/node/issues/6379>
   //  - <https://github.com/nodejs/node/issues/6456>
   //  - <https://github.com/nodejs/node/issues/2972>
   //  - …and many other near-duplicate issues
   //
-  // I've tried other workarounds such as passing a callback to `process.stdout.write` and ensuring
-  // the returned `Promise` is not resolved until it is called, explicitly calling
-  // calling `process.stdout.end` and/or `process.stdout.uncork`, etc and so far this is the only
-  // one that reliably results in the desired behavior.
+  // I've tried other workarounds such as explicitly terminating via `process.exit`, passing a
+  // callback to `process.stdout.write` (ensuring the returned `Promise` is not resolved until it
+  // is called), and explicitly calling `process.stdout.end`/`process.stdout.uncork` and so far
+  // this is the only thing which reliably results in the desired behavior.
   process.stdout.write('\n')
 }
 
