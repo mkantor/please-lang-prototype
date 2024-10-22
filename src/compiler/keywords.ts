d560759ea264aa4e127cbb8bd20617eb9990fada
Matt Kantor <the.matt.kantor@gmail.com>
2024-10-22 14:40:11 -0400

    Lightly refactor keywords.ts

--- src/compiler/keywords.ts
+++ src/compiler/keywords.ts
@@ -3,8 +3,10 @@ import * as either from '../adts/either.js'
 import type { Option } from '../adts/option.js'
 import * as option from '../adts/option.js'
 import { withPhantomData } from '../phantom-data.js'
+import type { Atom } from './atom.js'
 import type { KeywordError } from './errors.js'
 import type { CompiledAtom, CompiledMolecule } from './keyword-application.js'
+import type { Molecule } from './molecule.js'
 import type { KeywordsApplied } from './stages.js'
 
 type KeywordTransform = (
@@ -16,23 +18,14 @@ export type Keyword = 'check' | 'todo'
 /**
  * Checks whether a given value is assignable to a given type.
  */
-const check: KeywordTransform = configuration => {
-  const value = configuration.value ?? configuration['1']
-  const type = configuration.type ?? configuration['2']
-
-  if (value === undefined) {
-    return either.makeLeft({
-      kind: 'invalidKeywordArguments',
-      message:
-        'value must be provided via a property named `value` or the first positional argument',
-    })
-  } else if (type === undefined) {
-    return either.makeLeft({
-      kind: 'invalidKeywordArguments',
-      message:
-        'type must be provided via a property named `type` or the second positional argument',
-    })
-  } else if (value === type) {
+const check = ({
+  value,
+  type,
+}: {
+  readonly value: Atom | Molecule
+  readonly type: Atom | Molecule
+}): ReturnType<KeywordTransform> => {
+  if (value === type) {
     // This case is just an optimization. It allows skipping more expensive checks.
     return either.makeRight(
       option.makeSome(withPhantomData<KeywordsApplied>()(value)),
@@ -61,13 +54,10 @@ const check: KeywordTransform = configuration => {
         })
       } else {
         // Recursively check the property:
-        const resultOfCheckingProperty = check(
-          withPhantomData<KeywordsApplied>()({
-            0: '@check',
-            value: value[key],
-            type: typePropertyValue,
-          }),
-        )
+        const resultOfCheckingProperty = check({
+          value: value[key],
+          type: typePropertyValue,
+        })
         if (either.isLeft(resultOfCheckingProperty)) {
           return resultOfCheckingProperty
         }
@@ -81,7 +71,26 @@ const check: KeywordTransform = configuration => {
 }
 
 export const keywordTransforms: Record<Keyword, KeywordTransform> = {
-  check,
+  check: configuration => {
+    const value = configuration.value ?? configuration['1']
+    const type = configuration.type ?? configuration['2']
+
+    if (value === undefined) {
+      return either.makeLeft({
+        kind: 'invalidKeywordArguments',
+        message:
+          'value must be provided via a property named `value` or the first positional argument',
+      })
+    } else if (type === undefined) {
+      return either.makeLeft({
+        kind: 'invalidKeywordArguments',
+        message:
+          'type must be provided via a property named `type` or the second positional argument',
+      })
+    } else {
+      return check({ value, type })
+    }
+  },
   todo: _ => either.makeRight(option.none),
 }
 
