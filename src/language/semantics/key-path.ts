c426b16654ba29269d733dee0a8489b713a2ce24
Matt Kantor <the.matt.kantor@gmail.com>
2025-01-05 08:49:26 -0500

    Improve plz formatting in diagnostics

--- src/language/semantics/key-path.ts
+++ src/language/semantics/key-path.ts
@@ -1,19 +1,15 @@
 import { either } from '../../adts.js'
 import type { Atom, Molecule } from '../parsing.js'
 import { unparse } from '../unparsing.js'
-import { prettyPlz } from '../unparsing/pretty-plz.js'
+import { inlinePlz } from '../unparsing/inline-plz.js'
 
 export type KeyPath = readonly Atom[]
 
 export const stringifyKeyPathForEndUser = (keyPath: KeyPath): string =>
-  either.match(
-    // TODO: Use single-line plz notation.
-    unparse(keyPathToMolecule(keyPath), prettyPlz),
-    {
-      right: stringifiedOutput => stringifiedOutput,
-      left: error => `(unserializable key path: ${error.message})`,
-    },
-  )
+  either.match(unparse(keyPathToMolecule(keyPath), inlinePlz), {
+    right: stringifiedOutput => stringifiedOutput,
+    left: error => `(unserializable key path: ${error.message})`,
+  })
 
 export const keyPathToMolecule = (keyPath: KeyPath): Molecule =>
   Object.fromEntries(keyPath.flatMap((key, index) => [[index, key]]))
