f143d1fe414397be17550827e15dddb69b9e9ecf
Matt Kantor <the.matt.kantor@gmail.com>
2025-02-03 10:47:54 -0500

    Adopt new parse utility from @matt.kantor/parsing

--- src/language/parsing/parser.ts
+++ src/language/parsing/parser.ts
@@ -1,16 +1,10 @@
 import either, { type Either } from '@matt.kantor/either'
+import parsing from '@matt.kantor/parsing'
 import type { ParseError } from '../errors.js'
 import { type SyntaxTree, syntaxTreeParser } from './syntax-tree.js'
 
 export const parse = (input: string): Either<ParseError, SyntaxTree> =>
-  either.match(syntaxTreeParser(input.trim()), {
-    left: error => either.makeLeft({ ...error, kind: 'badSyntax' }),
-    right: ({ remainingInput, output }) =>
-      remainingInput.length !== 0
-        ? either.makeLeft({
-            kind: 'badSyntax',
-            message: 'excess content followed valid input',
-            remainingInput,
-          })
-        : either.makeRight(output),
-  })
+  either.mapLeft(parsing.parse(syntaxTreeParser, input.trim()), error => ({
+    ...error,
+    kind: 'badSyntax',
+  }))
