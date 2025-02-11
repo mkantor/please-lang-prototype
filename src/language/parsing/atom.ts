78727730952d19ccf7aa5ad14fd086ae3dbe6f3f
Matt Kantor <the.matt.kantor@gmail.com>
2025-02-11 13:05:05 -0500

    Drop unused `unit` constants

--- src/language/parsing/atom.ts
+++ src/language/parsing/atom.ts
@@ -19,8 +19,6 @@ export type Atom = string
 export const isAtom = (value: unknown): value is Atom =>
   typeof value === 'string'
 
-export const unit = '' as const
-
 const atomComponentsRequiringQuotation = [
   whitespace,
   literal('"'),
