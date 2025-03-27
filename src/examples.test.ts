a4c80cd725498d257a8c20521d6b19b9f114d2ec
Matt Kantor <the.matt.kantor@gmail.com>
2025-03-27 10:31:01 -0400

    Add snapshot testing for examples

--- src/examples.test.ts
+++ src/examples.test.ts
@@ -1,7 +1,26 @@
 import { exec } from 'node:child_process'
 import { promises as fs } from 'node:fs'
 import path from 'node:path'
-import test, { suite } from 'node:test'
+import test, { snapshot, suite } from 'node:test'
+
+// All examples will be tested with no additional command-line arguments. To
+// also test with specific sets of arguments, add them here:
+const additionalCommandLineArguments: Readonly<
+  Record<string, readonly string[]>
+> = {
+  'fibonacci.plz': [
+    '--input=0',
+    '--input=1',
+    '--input=2',
+    '--input=10',
+    '--input="not a number"',
+    '--input=-1',
+  ],
+}
+
+snapshot.setResolveSnapshotPath(_ =>
+  path.join(import.meta.dirname, '..', 'examples', '.snapshot'),
+)
 
 const exampleDirectoryPath = path.join(import.meta.dirname, '..', 'examples')
 const pleasePath = path.join(
@@ -16,24 +35,54 @@ suite('examples', async () => {
     fileName => fileName.endsWith('.plz'),
   )
   for (const exampleFileName of exampleFileNames) {
-    const exampleFilePath = path.join(exampleDirectoryPath, exampleFileName)
-    // TODO: Use snapshot testing instead of merely checking for errors.
-    test(
-      exampleFileName,
-      () =>
-        new Promise((resolve, reject) => {
-          const _childProcess = exec(
-            `cat "${exampleFilePath}" | node "${pleasePath}" --output-format=plz`,
-            (error, _stdout, _stderr) => {
-              // `error` is an `ExecError` when exit status is nonzero.
-              if (error !== null) {
-                reject(error)
-              } else {
-                resolve(undefined)
-              }
-            },
-          )
-        }),
-    )
+    const setsOfCommandLineArguments = [
+      '', // Always test with no arguments.
+      ...(additionalCommandLineArguments[exampleFileName] ?? []),
+    ] as const
+
+    for (const commandLineArguments of setsOfCommandLineArguments) {
+      const exampleFilePath = path.join(exampleDirectoryPath, exampleFileName)
+      test(
+        exampleFileName.concat(
+          commandLineArguments === '' ? '' : ` ${commandLineArguments}`,
+        ),
+        _ =>
+          new Promise((resolve, reject) => {
+            const _childProcess = exec(
+              `cat "${exampleFilePath}" | node "${pleasePath}" --no-color --output-format=plz ${commandLineArguments}`,
+              (error, stdout, stderr) => {
+                // `error` is an `ExecException` when exit status is nonzero.
+                if (error !== null) {
+                  reject(error)
+                } else {
+                  Promise.all([
+                    test('stdout', t =>
+                      t.assert.snapshot(stdout, snapshotOptions)),
+                    test('stderr', t =>
+                      t.assert.snapshot(stderr, snapshotOptions)),
+                  ])
+                    .then(_ => resolve(undefined))
+                    .catch(reject)
+                }
+              },
+            )
+          }),
+      )
+    }
   }
 })
+
+// Expect snapshots to already be strings.
+const snapshotOptions = {
+  serializers: [
+    (value: unknown) => {
+      if (typeof value !== 'string') {
+        throw new Error(
+          `snapshot was not a string (was \`${JSON.stringify(value)}\`)`,
+        )
+      } else {
+        return value
+      }
+    },
+  ],
+}
