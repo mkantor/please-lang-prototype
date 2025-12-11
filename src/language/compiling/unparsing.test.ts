15caaef8bc6abaf6640d6447d025482ef672edec
Matt Kantor <the.matt.kantor@gmail.com>
2025-12-11 11:39:41 -0500

    Refactor unparser tests

--- src/language/compiling/unparsing.test.ts
+++ src/language/compiling/unparsing.test.ts
@@ -9,252 +9,177 @@ import {
   type Notation,
 } from '../unparsing.js'
 
-const unparser = (notation: Notation) => (value: Atom | Molecule) =>
-  either.map(
-    typeof value === 'string'
-      ? notation.atom(value)
-      : notation.molecule(value, notation),
-    stripAnsi, // terminal styling is not currently tested
-  )
+const unparsers = (value: Atom | Molecule) => {
+  const unparse = (notation: Notation) =>
+    either.map(
+      typeof value === 'string'
+        ? notation.atom(value)
+        : notation.molecule(value, notation),
+      stripAnsi, // terminal styling is not currently tested
+    )
+  return {
+    inlinePlz: unparse(inlinePlz),
+    prettyPlz: unparse(prettyPlz),
+    prettyJson: unparse(prettyJson),
+  }
+}
 
-testCases(
-  unparser(inlinePlz),
-  input => `unparsing \`${JSON.stringify(input)}\``,
-)('inline plz', [
-  [{}, either.makeRight('{}')],
-  ['a', either.makeRight('a')],
-  ['Hello, world!', either.makeRight('"Hello, world!"')],
-  ['@test', either.makeRight('"@test"')],
-  [{ 0: 'a' }, either.makeRight('{ a }')],
-  [{ 1: 'a' }, either.makeRight('{ 1: a }')],
+const outputs = (
+  notations: Record<'inlinePlz' | 'prettyPlz' | 'prettyJson', string>,
+) => ({
+  inlinePlz: either.makeRight(notations.inlinePlz),
+  prettyPlz: either.makeRight(notations.prettyPlz),
+  prettyJson: either.makeRight(notations.prettyJson),
+})
+
+testCases(unparsers, input => `unparsing \`${JSON.stringify(input)}\``)(
+  'unparsing',
   [
-    { 0: 'a', 1: 'b', 3: 'c', somethingElse: 'd' },
-    either.makeRight('{ a, b, 3: c, somethingElse: d }'),
-  ],
-  [{ a: { b: { c: 'd' } } }, either.makeRight('{ a: { b: { c: d } } }')],
-  [
-    {
-      identity: {
-        0: '@function',
-        1: {
-          parameter: 'a',
-          body: { 0: '@lookup', 1: { 0: 'a' } },
-        },
-      },
-      test: {
-        0: '@apply',
-        1: {
-          function: { 0: '@lookup', 1: { 0: 'identity' } },
-          argument: 'it works!',
-        },
-      },
-    },
-    either.makeRight('{ identity: a => :a, test: :identity("it works!") }'),
-  ],
-  [
-    {
-      0: '@apply',
-      1: {
-        function: {
+    [{}, outputs({ inlinePlz: '{}', prettyPlz: '{}', prettyJson: '{}' })],
+    ['a', outputs({ inlinePlz: 'a', prettyPlz: 'a', prettyJson: '"a"' })],
+    [
+      'Hello, world!',
+      outputs({
+        inlinePlz: '"Hello, world!"',
+        prettyPlz: '"Hello, world!"',
+        prettyJson: '"Hello, world!"',
+      }),
+    ],
+    [
+      '@test',
+      outputs({
+        inlinePlz: '"@test"',
+        prettyPlz: '"@test"',
+        prettyJson: '"@test"',
+      }),
+    ],
+    [
+      { 0: 'a' },
+      outputs({
+        inlinePlz: '{ a }',
+        prettyPlz: '{\n  a\n}',
+        prettyJson: '{\n  "0": "a"\n}',
+      }),
+    ],
+    [
+      { 1: 'a' },
+      outputs({
+        inlinePlz: '{ 1: a }',
+        prettyPlz: '{\n  1: a\n}',
+        prettyJson: '{\n  "1": "a"\n}',
+      }),
+    ],
+    [
+      { 0: 'a', 1: 'b', 3: 'c', somethingElse: 'd' },
+      outputs({
+        inlinePlz: '{ a, b, 3: c, somethingElse: d }',
+        prettyPlz: '{\n  a\n  b\n  3: c\n  somethingElse: d\n}',
+        prettyJson:
+          '{\n  "0": "a",\n  "1": "b",\n  "3": "c",\n  "somethingElse": "d"\n}',
+      }),
+    ],
+    [
+      { a: { b: { c: 'd' } } },
+      outputs({
+        inlinePlz: '{ a: { b: { c: d } } }',
+        prettyPlz: '{\n  a: {\n    b: {\n      c: d\n    }\n  }\n}',
+        prettyJson: '{\n  "a": {\n    "b": {\n      "c": "d"\n    }\n  }\n}',
+      }),
+    ],
+    [
+      {
+        identity: {
           0: '@function',
           1: {
             parameter: 'a',
             body: { 0: '@lookup', 1: { 0: 'a' } },
           },
         },
-        argument: 'it works!',
-      },
-    },
-    either.makeRight('(a => :a)("it works!")'),
-  ],
-  [
-    {
-      0: '@runtime',
-      1: {
-        0: {
-          0: '@function',
+        test: {
+          0: '@apply',
           1: {
-            parameter: 'context',
-            body: {
-              0: '@index',
-              1: {
-                object: { 0: '@lookup', 1: { key: 'context' } },
-                query: { 0: 'program', 1: 'start_time' },
+            function: { 0: '@lookup', 1: { 0: 'identity' } },
+            argument: 'it works!',
+          },
+        },
+      },
+      outputs({
+        inlinePlz: '{ identity: a => :a, test: :identity("it works!") }',
+        prettyPlz: '{\n  identity: a => :a\n  test: :identity("it works!")\n}',
+        prettyJson:
+          '{\n  "identity": {\n    "0": "@function",\n    "1": {\n      "parameter": "a",\n      "body": {\n        "0": "@lookup",\n        "1": {\n          "0": "a"\n        }\n      }\n    }\n  },\n  "test": {\n    "0": "@apply",\n    "1": {\n      "function": {\n        "0": "@lookup",\n        "1": {\n          "0": "identity"\n        }\n      },\n      "argument": "it works!"\n    }\n  }\n}',
+      }),
+    ],
+    [
+      {
+        0: '@apply',
+        1: {
+          function: {
+            0: '@function',
+            1: {
+              parameter: 'a',
+              body: { 0: '@lookup', 1: { 0: 'a' } },
+            },
+          },
+          argument: 'it works!',
+        },
+      },
+      outputs({
+        inlinePlz: '(a => :a)("it works!")',
+        prettyPlz: '(a => :a)("it works!")',
+        prettyJson:
+          '{\n  "0": "@apply",\n  "1": {\n    "function": {\n      "0": "@function",\n      "1": {\n        "parameter": "a",\n        "body": {\n          "0": "@lookup",\n          "1": {\n            "0": "a"\n          }\n        }\n      }\n    },\n    "argument": "it works!"\n  }\n}',
+      }),
+    ],
+    [
+      {
+        0: '@runtime',
+        1: {
+          0: {
+            0: '@function',
+            1: {
+              parameter: 'context',
+              body: {
+                0: '@index',
+                1: {
+                  object: { 0: '@lookup', 1: { key: 'context' } },
+                  query: { 0: 'program', 1: 'start_time' },
+                },
               },
             },
           },
         },
       },
-    },
-    either.makeRight('@runtime { context => :context.program.start_time }'),
-  ],
-  [
-    {
-      'a.b': {
-        'c "d"': {
-          'e.f': 'g',
-        },
-      },
-      test: {
-        0: '@index',
-        1: {
-          object: { 0: '@lookup', 1: { 0: 'a.b' } },
-          query: { 0: 'c "d"', 1: 'e.f' },
-        },
-      },
-    },
-    either.makeRight(
-      '{ a.b: { "c \\"d"": { e.f: g } }, test: :"a.b"."c \\"d""."e.f" }',
-    ),
-  ],
-])
-
-testCases(
-  unparser(prettyPlz),
-  input => `unparsing \`${JSON.stringify(input)}\``,
-)('pretty plz', [
-  [{}, either.makeRight('{}')],
-  ['a', either.makeRight('a')],
-  ['Hello, world!', either.makeRight('"Hello, world!"')],
-  ['@test', either.makeRight('"@test"')],
-  [{ 0: 'a' }, either.makeRight('{\n  a\n}')],
-  [{ 1: 'a' }, either.makeRight('{\n  1: a\n}')],
-  [
-    { 0: 'a', 1: 'b', 3: 'c', somethingElse: 'd' },
-    either.makeRight('{\n  a\n  b\n  3: c\n  somethingElse: d\n}'),
-  ],
-  [
-    { a: { b: { c: 'd' } } },
-    either.makeRight('{\n  a: {\n    b: {\n      c: d\n    }\n  }\n}'),
-  ],
-  [
-    {
-      identity: {
-        0: '@function',
-        1: { parameter: 'a', body: { 0: '@lookup', 1: { 0: 'a' } } },
-      },
-      test: {
-        0: '@apply',
-        1: {
-          function: { 0: '@lookup', 1: { 0: 'identity' } },
-          argument: 'it works!',
-        },
-      },
-    },
-    either.makeRight(
-      '{\n  identity: a => :a\n  test: :identity("it works!")\n}',
-    ),
-  ],
-  [
-    {
-      0: '@apply',
-      1: {
-        function: {
-          0: '@function',
-          1: {
-            parameter: 'a',
-            body: { 0: '@lookup', 1: { 0: 'a' } },
+      outputs({
+        inlinePlz: '@runtime { context => :context.program.start_time }',
+        prettyPlz: '@runtime {\n  context => :context.program.start_time\n}',
+        prettyJson:
+          '{\n  "0": "@runtime",\n  "1": {\n    "0": {\n      "0": "@function",\n      "1": {\n        "parameter": "context",\n        "body": {\n          "0": "@index",\n          "1": {\n            "object": {\n              "0": "@lookup",\n              "1": {\n                "key": "context"\n              }\n            },\n            "query": {\n              "0": "program",\n              "1": "start_time"\n            }\n          }\n        }\n      }\n    }\n  }\n}',
+      }),
+    ],
+    [
+      {
+        'a.b': {
+          'c "d"': {
+            'e.f': 'g',
           },
         },
-        argument: 'it works!',
-      },
-    },
-    either.makeRight('(a => :a)("it works!")'),
-  ],
-  [
-    {
-      0: '@runtime',
-      1: {
-        0: {
-          0: '@function',
+        test: {
+          0: '@index',
           1: {
-            parameter: 'context',
-            body: {
-              0: '@index',
-              1: {
-                object: { 0: '@lookup', 1: { key: 'context' } },
-                query: { 0: 'program', 1: 'start_time' },
-              },
-            },
+            object: { 0: '@lookup', 1: { 0: 'a.b' } },
+            query: { 0: 'c "d"', 1: 'e.f' },
           },
         },
       },
-    },
-    either.makeRight('@runtime {\n  context => :context.program.start_time\n}'),
+      outputs({
+        inlinePlz:
+          '{ a.b: { "c \\"d"": { e.f: g } }, test: :"a.b"."c \\"d""."e.f" }',
+        prettyPlz:
+          '{\n  a.b: {\n    "c \\"d"": {\n      e.f: g\n    }\n  }\n  test: :"a.b"."c \\"d""."e.f"\n}',
+        prettyJson:
+          '{\n  "a.b": {\n    "c \\"d"": {\n      "e.f": "g"\n    }\n  },\n  "test": {\n    "0": "@index",\n    "1": {\n      "object": {\n        "0": "@lookup",\n        "1": {\n          "0": "a.b"\n        }\n      },\n      "query": {\n        "0": "c \\"d"",\n        "1": "e.f"\n      }\n    }\n  }\n}',
+      }),
+    ],
   ],
-  [
-    {
-      'a.b': {
-        'c "d"': {
-          'e.f': 'g',
-        },
-      },
-      test: {
-        0: '@index',
-        1: {
-          object: { 0: '@lookup', 1: { 0: 'a.b' } },
-          query: { 0: 'c "d"', 1: 'e.f' },
-        },
-      },
-    },
-    either.makeRight(
-      '{\n  a.b: {\n    "c \\"d"": {\n      e.f: g\n    }\n  }\n  test: :"a.b"."c \\"d""."e.f"\n}',
-    ),
-  ],
-])
-
-testCases(
-  unparser(prettyJson),
-  input => `unparsing \`${JSON.stringify(input)}\``,
-)('pretty JSON', [
-  [{}, either.makeRight('{}')],
-  ['a', either.makeRight('"a"')],
-  ['Hello, world!', either.makeRight('"Hello, world!"')],
-  ['@test', either.makeRight('"@test"')],
-  [{ 0: 'a' }, either.makeRight('{\n  "0": "a"\n}')],
-  [{ 1: 'a' }, either.makeRight('{\n  "1": "a"\n}')],
-  [
-    { 0: 'a', 1: 'b', 3: 'c', somethingElse: 'd' },
-    either.makeRight(
-      '{\n  "0": "a",\n  "1": "b",\n  "3": "c",\n  "somethingElse": "d"\n}',
-    ),
-  ],
-  [
-    { a: { b: { c: 'd' } } },
-    either.makeRight('{\n  "a": {\n    "b": {\n      "c": "d"\n    }\n  }\n}'),
-  ],
-  [
-    {
-      identity: {
-        0: '@function',
-        1: { parameter: 'a', body: { 0: '@lookup', 1: { 0: 'a' } } },
-      },
-      test: {
-        0: '@apply',
-        1: {
-          function: { 0: '@lookup', 1: { 0: 'identity' } },
-          argument: 'it works!',
-        },
-      },
-    },
-    either.makeRight(
-      '{\n  "identity": {\n    "0": "@function",\n    "1": {\n      "parameter": "a",\n      "body": {\n        "0": "@lookup",\n        "1": {\n          "0": "a"\n        }\n      }\n    }\n  },\n  "test": {\n    "0": "@apply",\n    "1": {\n      "function": {\n        "0": "@lookup",\n        "1": {\n          "0": "identity"\n        }\n      },\n      "argument": "it works!"\n    }\n  }\n}',
-    ),
-  ],
-  [
-    {
-      0: '@apply',
-      function: {
-        0: '@function',
-        1: {
-          parameter: 'a',
-          body: { 0: '@lookup', 1: { 0: 'a' } },
-        },
-      },
-      argument: 'it works!',
-    },
-    either.makeRight(
-      '{\n  "0": "@apply",\n  "function": {\n    "0": "@function",\n    "1": {\n      "parameter": "a",\n      "body": {\n        "0": "@lookup",\n        "1": {\n          "0": "a"\n        }\n      }\n    }\n  },\n  "argument": "it works!"\n}',
-    ),
-  ],
-])
+)
