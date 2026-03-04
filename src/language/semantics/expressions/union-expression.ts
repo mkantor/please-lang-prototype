7126600174a920743e720a3fb76dd393c8b2b906
Matt Kantor <the.matt.kantor@gmail.com>
2026-03-04 07:11:53 -0500

    Add more utility functions in union-expression.ts

--- src/language/semantics/expressions/union-expression.ts
+++ src/language/semantics/expressions/union-expression.ts
@@ -1,4 +1,9 @@
+import either, { type Either } from '@matt.kantor/either'
+import type { ElaborationError } from '../../errors.js'
+import { isKeywordExpressionWithArgument } from '../expression.js'
 import type { ObjectNode } from '../object-node.js'
+import { isObjectNode, makeObjectNode } from '../object-node.js'
+import type { SemanticGraph } from '../semantic-graph.js'
 
 export type UnionExpression = ObjectNode & {
   readonly 0: '@union'
@@ -8,3 +13,19 @@ export type UnionExpression = ObjectNode & {
    */
   readonly 1: ObjectNode
 }
+
+export const readUnionExpression = (
+  node: SemanticGraph,
+): Either<ElaborationError, UnionExpression> =>
+  isKeywordExpressionWithArgument('@union', node) && isObjectNode(node['1']) ?
+    either.makeRight(makeUnionExpression(node['1']))
+  : either.makeLeft({
+      kind: 'invalidExpression',
+      message: 'not a `@union` expression',
+    })
+
+export const makeUnionExpression = (members: ObjectNode): UnionExpression =>
+  makeObjectNode({
+    0: '@union',
+    1: members,
+  })
