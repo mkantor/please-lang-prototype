b97b0a53ec2c46ba025ff1bb2dcf2f1ad0da474c
Matt Kantor <the.matt.kantor@gmail.com>
2026-04-06 08:48:38 -0400

    Add `inlineSugarFreePrettyPlz` notation

---
+++ src/language/unparsing/inline-sugar-free-pretty-plz.ts
@@ -0,0 +1,47 @@
+import either from '@matt.kantor/either'
+import { styleText } from 'node:util'
+import type { Molecule } from '../parsing.js'
+import {
+  moleculeAsKeyValuePairStrings,
+  unparseAtom,
+  type SemanticContext,
+  type UnparseAtomOrMolecule,
+} from './plz-utilities.js'
+import { punctuation, type Notation } from './unparsing-utilities.js'
+
+const unparseMolecule =
+  (semanticContext: SemanticContext) => (value: Molecule) => {
+    const { closeBrace, openBrace, comma } = punctuation(styleText)
+    if (Object.keys(value).length === 0) {
+      return either.makeRight(openBrace + closeBrace)
+    } else {
+      return either.map(
+        moleculeAsKeyValuePairStrings(
+          value,
+          { unparseAtomOrMolecule, semanticContext },
+          {
+            ordinalKeys: 'preserve',
+          },
+        ),
+        keyValuePairsAsStrings =>
+          openBrace.concat(
+            ' ',
+            keyValuePairsAsStrings.join(comma.concat(' ')),
+            ' ',
+            closeBrace,
+          ),
+      )
+    }
+  }
+
+const unparseAtomOrMolecule: UnparseAtomOrMolecule =
+  semanticContext => value =>
+    typeof value === 'string' ?
+      unparseAtom(value)
+    : unparseMolecule(semanticContext)(value)
+
+export const inlineSugarFreePrettyPlz: Notation = {
+  atom: unparseAtom,
+  molecule: unparseMolecule('default'),
+  suffix: '',
+}
