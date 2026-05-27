1399826df13d9f5ea70ba32e429f3684d8eee476
Matt Kantor <the.matt.kantor@gmail.com>
2026-05-27 12:31:29 -0400

    Default to `--output-format=plz` in `please` executable

--- src/language/cli/please.ts
+++ src/language/cli/please.ts
@@ -2,19 +2,24 @@ import either from '@matt.kantor/either'
 import { compile } from '../compiling.js'
 import { parse } from '../parsing/parser.js'
 import { evaluate } from '../runtime.js'
+import { prettyPlz } from '../unparsing.js'
 import { readString } from './input.js'
 import { handleOutput } from './output.js'
 
 const main = async (process: NodeJS.Process): Promise<undefined> =>
-  handleOutput(process, async () => {
-    const sourceCode = await readString(process.stdin)
+  handleOutput(
+    process,
+    async () => {
+      const sourceCode = await readString(process.stdin)
 
-    // TODO: Cache intermediate representations to the filesystem.
-    const syntaxTree = parse(sourceCode)
-    const program = either.flatMap(syntaxTree, compile)
-    const runtimeOutput = either.flatMap(program, evaluate)
+      // TODO: Cache intermediate representations to the filesystem.
+      const syntaxTree = parse(sourceCode)
+      const program = either.flatMap(syntaxTree, compile)
+      const runtimeOutput = either.flatMap(program, evaluate)
 
-    return runtimeOutput
-  })
+      return runtimeOutput
+    },
+    prettyPlz,
+  )
 
 await main(process)
